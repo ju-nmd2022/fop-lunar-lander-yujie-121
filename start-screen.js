9cf5fb768f97a51b95f9a3bf898e6df2975e5a75
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2023-03-01 23:39:32 +0100

    add result screen adn re-start button

--- start-screen.js
+++ start-screen.js
@@ -4,15 +4,19 @@ function startScreen() {
   let noteText = "Press down the SPACE key to decelerate! ";
   fill(0, 0, 0);
   textSize(18);
-  text(noteText, 170, 330);
+  text(noteText, 170, 230);
   let note1Text = "LEFT and RIGHT key to moving! ";
   fill(0, 0, 0);
   textSize(18);
-  text(note1Text, 190, 380);
+  text(note1Text, 190, 280);
+
+  fill(0, 0, 0);
+  textSize(40);
+  text("Smoothly Landing Challenge ", 80, 150);
   push();
   fill(255, 140, 0);
-  textSize(35);
-  text("⚠", 130, 330);
+  textSize(30);
+  text("⚠", 130, 230);
   pop();
   pop();
 }
@@ -23,12 +27,12 @@ function startButton() {
   push();
   stroke(70, 130, 180);
   fill(173, 216, 230);
-  rect(217, 200, 200, 70, 30);
+  rect(217, 350, 200, 70, 30);
   pop();
   let startText = "Click start";
   fill(255, 255, 255);
   textSize(38);
-  text(startText, 234, 250);
+  text(startText, 234, 400);
   pop();
 }
 
@@ -38,7 +42,7 @@ function draw() {
   startScreen();
 }
 function mouseClicked() {
-  if (mouseX > 217 && mouseX < 217 + 200 && mouseY > 200 && mouseY < 200 + 70) {
+  if (mouseX > 217 && mouseX < 217 + 200 && mouseY > 350 && mouseY < 350 + 70) {
     console.log("button clicked");
   }
   //stage =="game"
